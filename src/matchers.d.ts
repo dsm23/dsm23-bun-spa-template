7468f7166799fd3dcf019025476a21c93ec30bb4
David Murdoch <dsmurdoch357@gmail.com>
2025-11-30 15:26:28 +0000

    chore(deps): update dependency prettier to v3.7.1

--- src/matchers.d.ts
+++ src/matchers.d.ts
@@ -2,8 +2,10 @@ import type { TestingLibraryMatchers } from "@testing-library/jest-dom/matchers"
 
 /* eslint-disable @typescript-eslint/no-empty-object-type */
 declare module "bun:test" {
-  interface Matchers<T>
-    extends TestingLibraryMatchers<typeof expect.stringContaining, T> {}
+  interface Matchers<T> extends TestingLibraryMatchers<
+    typeof expect.stringContaining,
+    T
+  > {}
   interface AsymmetricMatchers extends TestingLibraryMatchers {}
 }
 /* eslint-enable @typescript-eslint/no-empty-object-type */
